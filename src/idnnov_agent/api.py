be1cc78af655aaf224a81842b33cdd7a5b98ae6c
Rock Langlois <0@z0.gg>
2026-09-03 15:14:24 -0400

    fix: order OpenObserve save before test

--- src/idnnov_agent/api.py
+++ src/idnnov_agent/api.py
@@ -44,7 +44,7 @@ def get_status():
     settings = persistence.load_public(ETC)
     return {
         "running": running,
-        "package_version": "1.1.2-1015",
+        "package_version": "1.1.3-1016",
         "fluent_bit_version": "5.0.9",
         "destination": settings["collector_url"],
         "organization": settings["organization"],
